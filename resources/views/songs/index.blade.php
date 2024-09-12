467441fb816c57b02d2162e23506cc86dcea2482
unknown <you@example.com>
2024-09-12 14:13:24 +0300

    Added nice looking css to songs.index

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -6,20 +6,38 @@
         }
 
         .grid-item{
-            background-color: gray;
-            text-align: center;
+            transition: 0.3s;
+	        background-color: rgb(243 244 246);
+
+            border-radius: 8px;
             display: block;
-            margin-left: 0px;
-            margin-right: 0px;
-            margin-top: 16px;
-            margin-bottom: 16px;
+            margin-left: 8px;
+            margin-right: 8px;
+            margin-top: 8px;
+            margin-bottom: 8px;
+            padding-top: 20px;
+            padding-bottom: 16px;
         }
+
+        .grid-item:hover{
+            background-color: #DFDFDF;
+        }
+
 
         .song-image {
             width: 256px;
             height: 256px;
             display: block;
-            margin-left: 32px;
+            margin-left: 21.5px;
+            margin-top: 0px
+        }
+
+        .song-title, .song-artist {
+            margin-left: 21.5px;
+        }
+
+        .song-title{
+            font-size: 20px
         }
 
 
@@ -32,21 +50,20 @@
             <span>Create Song</span>
         </a>
     </div>
+
+
+
     <div class="grid-container">
         @foreach ($songs as $song)
+        <a href="{{ route('songs.show', $song->id) }}">
         <div class="grid-item">
                 <img src="/images/test_img.png" alt="test image" class="song-image">  
 
-                <h1 class="song-title">
-                    {{ $song->title }}
-                </h1>
+                <h1 class="song-title"><b>{{ $song->title }}</b></h1>
 
-                <p class="">
-                    {{ $song->artist }}
-                </p>
-
-
+                <p class="song-artist">{{ $song->artist }}</p>
         </div>
+        </a>
         @endforeach
     </div>
 </x-app-layout>
